73b57b6185c0d6d6b74c3e2adf13977d10cdd28c
lanewells <wellsdelaneyk@gmail.com>
2024-09-23 15:59:36 -0700

    Completed .js files of levels one, two, and three

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,24 @@
  * DOTS: Level Three
  *
  */
+let score = 0
+let ball = document.querySelectorAll('.js-ball')
+let scoreDisplay = document.querySelector('.js-score')
+let levelWinner = document.querySelector('.level-winner')
+
+//Using a for Loop
+
+// for (let i = 0; i < ball.length; i++) {
+// ball[i].addEventListener('click', function() {
+//     score += parseInt(this.getAttribute('data-increment'))
+//     scoreDisplay.innerText = score
+//     if (score >= 100) {levelWinner.style.opacity = 1}
+// })}
+
+//Using a forEach Loop
+
+ball.forEach((ballito) => ballito.addEventListener('click', function() {
+    score += parseInt(this.getAttribute('data-increment'))
+    scoreDisplay.innerText = score
+    if (score >= 100) {levelWinner.style.opacity = 1}
+}))
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,14 @@
  * DOTS: Level Two
  *
  */
+let score = 0
+let ball = document.querySelectorAll('.js-ball')
+let scoreDisplay = document.querySelector('.js-score')
+let levelWinner = document.querySelector('.level-winner')
+
+for (let i = 0; i < ball.length; i++) {
+ball[i].addEventListener('click', function() {
+    score += 10
+    scoreDisplay.innerText = score
+    if (score >= 100) {levelWinner.style.opacity = 1}
+})}
